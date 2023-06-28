bf9293a53a39226ec0f7388486dc7b98e92bfc77
s5hy25ni <s5hy25ni@gmail.com>
2023-06-29 02:36:06 +0900

    login 성공

--- web_project/src/main/java/com/hr/pro/ctrl/Controller_admin.java
+++ web_project/src/main/java/com/hr/pro/ctrl/Controller_admin.java
@@ -8,11 +8,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.common.hash.Hashing;
 import com.hr.pro.dto.Admin_DTO;
@@ -30,31 +32,33 @@ public class Controller_admin extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String id = req.getParameter("user_id");
 		String pw = req.getParameter("user_pw");
+
+		String pwHash = Hashing.sha256().hashString(pw, StandardCharsets.UTF_8).toString();
 		
-		System.out.println(id + pw);
-//		String pwHash = Hashing.sha256().hashString(pw, StandardCharsets.UTF_8).toString();
-//		
-//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		Calendar cal = Calendar.getInstance();
-//		String now = format.format(cal.getTime());
-//		
-//		Map<String, Object> loginUser = new HashMap<String, Object>();
-//		loginUser.put("admin_id", id);
-//		loginUser.put("admin_pw", pwHash);
-//		loginUser.put("last_login", now);
-//		
-//		IAdminDao dao = new AdminDaoImpl();
-//		List<Admin_DTO> loginSuccess = dao.getLoginUser(loginUser);
-//		
-//		if(loginSuccess.size()!=0) {
-//			req.setAttribute("loginSuccess", "true");
-//			req.setAttribute("userInfo", dao);
-//			dao.setLastLogin(loginUser);
-//		} else {
-//			req.setAttribute("loginSuccess", "false");
-//		}
-//		
-//		req.getRequestDispatcher("/index.jsp").forward(req, resp);
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		String now = format.format(cal.getTime());
+		
+		Map<String, Object> loginUser = new HashMap<String, Object>();
+		loginUser.put("admin_id", id);
+		loginUser.put("admin_pw", pwHash);
+		loginUser.put("last_login", now);
+		
+		IAdminDao dao = new AdminDaoImpl();
+		List<Admin_DTO> loginSuccess = dao.getLoginUser(loginUser);
+		
+		HttpSession session = req.getSession();
+
+		if(loginSuccess.size()!=0) {
+			req.setAttribute("loginSuccess", "true");
+			session.setAttribute("id", id);
+			session.setAttribute("lastLogin", now);
+			dao.setLastLogin(loginUser);
+		} else {
+			req.setAttribute("loginSuccess", "false");
+		}
+		
+		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 
 }
