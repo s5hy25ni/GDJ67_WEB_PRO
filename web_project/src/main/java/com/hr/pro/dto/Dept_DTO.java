1b2b0126c1175cb69aada8d187f9365f53ac14c2
s5hy25ni <s5hy25ni@gmail.com>
2023-07-02 18:52:49 +0900

    충돌..?

--- web_project/src/main/java/com/hr/pro/dto/Dept_DTO.java
+++ web_project/src/main/java/com/hr/pro/dto/Dept_DTO.java
@@ -1,14 +1,5 @@
 package com.hr.pro.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-
-
 public class Dept_DTO {
 	
 	private int department_id;
