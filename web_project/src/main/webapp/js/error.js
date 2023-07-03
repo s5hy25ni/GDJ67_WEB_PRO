4f8bd8a1f3738bdd438c2f26f78027b8eb34a2a7
s5hy25ni <s5hy25ni@gmail.com>
2023-07-03 21:43:55 +0900

    error 페이지 완료

---
+++ web_project/src/main/webapp/js/error.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+ document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("btn").addEventListener("click", function(){
+		location.href="http://localhost:8080/web_project";
+	});
+});
